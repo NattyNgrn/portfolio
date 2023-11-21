08d47bbf8925f666b64e40fa3c096ffd8c9a5e5b
NattyNgrn <natnrgn@gmail.com>
2023-11-21 15:38:01 -0500

    set up

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-import { Inter } from 'next/font/google'
+
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={"bg-blue-950 font-[Urbanist] text-4xl text-indigo-300"}>{children}</body>
     </html>
   )
 }
